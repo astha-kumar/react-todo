d14fb8e4c9ceff7c3f0b320e63224d0e2d72cb82
Astha Kumar <asthakumar03@gmail.com>
2020-08-02 16:16:02 +0530

    Updates

---
+++ precache-manifest.a7f7ebe6d6a29406a992da8f66f7c8a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b956b9f4c1cdc6162db10cfbd5d86f6a",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "c2a0e8df0b9f3d9854d7",
+    "url": "/react-todo/static/css/main.a70a301a.chunk.css"
+  },
+  {
+    "revision": "1dc324bd780aa2526f72",
+    "url": "/react-todo/static/js/2.eb172f68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.eb172f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a0e8df0b9f3d9854d7",
+    "url": "/react-todo/static/js/main.6811cd6a.chunk.js"
+  },
+  {
+    "revision": "c80c04419047b90ba603",
+    "url": "/react-todo/static/js/runtime-main.dca37b03.js"
+  },
+  {
+    "revision": "53448e0a0cb4c12ff402826b5872fffe",
+    "url": "/react-todo/static/media/logoInPng.53448e0a.png"
+  },
+  {
+    "revision": "3483beb8be98271c5c93e5cc2a29fc9d",
+    "url": "/react-todo/static/media/rain.3483beb8.png"
+  }
+]);
